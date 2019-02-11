21c46487c2b9aed95efd6dea0714eec87073cb37
hemanth <hemanth.shendye@gmail.com>
2019-02-11 11:24:05 +0530

    final copy

--- animate.js
+++ animate.js
@@ -1,7 +1,6 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -15,7 +14,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -24,27 +23,29 @@ $(document).ready(function(){
   });
 
 
-  $(function(){
- 
-    $(document).on( 'scroll', function(){
- 
-    	if ($(window).scrollTop() > 100) {
-			$('.scroll-top-wrapper').addClass('show');
-		} else {
-			$('.scroll-top-wrapper').removeClass('show');
-		}
-	});
- 
-	$('.scroll-top-wrapper').on('click', scrollToTop);
-});
- 
-function scrollToTop() {
-	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-	element = $('body');
-	offset = element.offset();
-	offsetTop = offset.top;
-	$('html, body').animate({scrollTop: offsetTop}, 1500, 'linear');
-}
+  $(function () {
+
+    $(document).on('scroll', function () {
+
+      if ($(window).scrollTop() > 100) {
+        $('.scroll-top-wrapper').addClass('show');
+      } else {
+        $('.scroll-top-wrapper').removeClass('show');
+      }
+    });
+
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+  });
+
+  function scrollToTop() {
+    verticalOffset = typeof (verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({
+      scrollTop: offsetTop
+    }, 500, 'linear');
+  }
 
 
 
@@ -52,11 +53,12 @@ function scrollToTop() {
 });
 
 //for menu hide
-  
-$(function(){ 
+
+$(function () {
   var navMain = $(".navbar-collapse");
 
   navMain.on("click", "a", null, function () {
-      navMain.collapse('hide');
+    navMain.collapse('hide');
   });
 });
+
